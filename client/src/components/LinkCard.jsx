42a45b800b470038cc23365fa6ac34d8aad24751
Konstantin <picprofit@gmail.com>
2020-04-18 20:26:53 +0300

    add: Link pages

---
+++ client/src/components/LinkCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LinkCard = ({ link }) => {
+  return <>
+    <h2>Link</h2>
+    <p>Short: {link}</p>
+    <p>Full: {link}</p>
+    </>;
+};
+
+export default LinkCard;
--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const CreatePage = () => {
+import useHttp from '../hooks/http.hook';
+import Loader from '../components/Loader';
+import LinkCard from '../components/LinkCard';
+import AuthContext from '../context/AuthContext';
+
+const DetailPage = () => {
+  const { token } = useContext(AuthContext);
+  const { request, loading } = useHttp();
+  const [link, setLink] = useState(null);
+  const { id: linkId } = useParams();
+
+  const getLink = useCallback(async () => {
+    try {
+      const fetched = await request(`/api/link/${linkId}`, 'GET', null, {
+        Authorization: `Bearer ${token}`
+      });
+      setLink(fetched);
+    } catch (e) {}
+  }, [token, linkId, request]);
+
+  useEffect(() => {
+    getLink();
+  }, [getLink]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-    <div>
+    <>
       <h1>Detail page</h1>
-      
-    </div>
+      {link && <LinkCard link={link} />}
+    </>
   );
 };
 
-export default CreatePage;
+export default DetailPage;
