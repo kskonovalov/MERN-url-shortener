09328bfcc5a46dba441dab4654de6509fad6fde7
Konstantin <picprofit@gmail.com>
2020-04-18 18:19:46 +0300

    add: links backend

---
+++ routes/link.routes.js
@@ -0,0 +1,53 @@
+const { Router } = require('express');
+const config = require('config');
+const shortid = require('shortid');
+const Link = require('../models/Link');
+const auth = require('../middleware/auth.middleware');
+
+const router = Router();
+
+router.post('/generate', auth, async (req, res) => {
+  try {
+    const baseUrl = config.get('baseUrl');
+    const { from } = req.body;
+
+    const code = shortid.generate();
+    const exists = await Link.findOne({ from });
+    if (exists) {
+      return res.json({ link: exists });
+    }
+
+    const to = baseUrl + '/t/' + code;
+
+    const link = new Link({
+      code,
+      to,
+      from,
+      owner: req.user.userId
+    });
+
+    await link.save();
+
+    res.status(201).json({ link });
+  } catch (e) {
+    res.status(500).json({ message: 'Something get wrong! Please try again' });
+  }
+});
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const links = await Link.find({ owner: req.user.userId });
+  } catch (e) {
+    res.status(500).json({ message: 'Something get wrong! Please try again' });
+  }
+});
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const links = await Link.findById(req.params.id);
+  } catch (e) {
+    res.status(500).json({ message: 'Something get wrong! Please try again' });
+  }
+});
+
+module.exports = router;
